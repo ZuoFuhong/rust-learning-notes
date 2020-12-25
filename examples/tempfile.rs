ba16ba72fb1dccd1d012dc0b29ab4d87e994c0b9
zfh <fuhongzuo@gmail.com>
2020-12-25 09:58:08 +0800

    临时文件

--- examples/tempfile.rs
+++ examples/tempfile.rs
@@ -1,9 +1,19 @@
-use std::env::temp_dir;
+use std::fs::File;
 use std::io::Write;
-use tempfile::tempfile;
+use tempfile::{tempdir, tempfile};
 
 fn main() {
-    // Create a file insideof `std::env::temp_dir()`
+    println!("std::env::temp_dir = {:?}", std::env::temp_dir());
+    // Create a file inside of `std::env::temp_dir()`
     let mut file = tempfile().unwrap();
     writeln!(file, "Brian was here. Briefly.").unwrap();
+
+    // Create a directory inside of `std::env::temp_dir()`
+    let dir = tempdir().unwrap();
+    let file_path = dir.path().join("temporary-note.txt");
+    let mut file = File::create(file_path).unwrap();
+    writeln!(file, "Brian was here. Briefly.").unwrap();
+
+    drop(file);
+    dir.close().unwrap();
 }
