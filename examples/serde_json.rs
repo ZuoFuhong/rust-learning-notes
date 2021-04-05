f41259d314a3d53ae41c443f8f13e9879a60136e
zfh <fuhongzuo@gmail.com>
2021-04-05 16:50:53 +0800

    learn serde_json

--- examples/serde_json.rs
+++ examples/serde_json.rs
@@ -1,12 +1,15 @@
 use serde::{Deserialize, Serialize};
-use serde_json::{Result, Value};
+use serde_json::{Deserializer, Value};
+use std::collections::HashMap;
 
 fn main() {
-    untyped_example().unwrap();
-    typed_example().unwrap();
+    untyped_example();
+    typed_example();
+    hashmap_example();
+    stream_into_iter();
 }
 
-fn untyped_example() -> Result<()> {
+fn untyped_example() {
     let data = r#"
         {
             "name": "mars",
@@ -17,12 +20,11 @@ fn untyped_example() -> Result<()> {
             ]
         }    
     "#;
-    let v: Value = serde_json::from_str(data)?;
+    let v: Value = serde_json::from_str(data).unwrap();
     println!(
         "name = {}, first phone number = {}",
         v["name"], v["phones"][0]
     );
-    Ok(())
 }
 
 #[derive(Serialize, Deserialize)]
@@ -32,7 +34,7 @@ struct Person {
     phones: Vec<String>,
 }
 
-fn typed_example() -> Result<()> {
+fn typed_example() {
     let data = r#"
         {
             "name": "mars",
@@ -43,9 +45,33 @@ fn typed_example() -> Result<()> {
             ]
         }
     "#;
-    let p: Person = serde_json::from_str(data)?;
+    let p: Person = serde_json::from_str(data).unwrap();
     println!("name = {}, first phone number = {}", p.name, p.phones[0]);
-    let s = serde_json::to_string(&p)?;
+    let s = serde_json::to_string(&p).unwrap();
     println!("{}", s);
-    Ok(())
+}
+
+#[derive(Deserialize, Debug)]
+struct Data {
+    foo: String,
+}
+
+fn hashmap_example() {
+    let data = r#"{
+        "a": {"foo": "bar"},
+        "b": {"foo": "baz"}
+    }"#;
+    let m: HashMap<String, Data> = serde_json::from_str(data).unwrap();
+    for (k, v) in m {
+        println!("k = {}, v = {:?}", k, v)
+    }
+}
+
+// 将流反序列化为多个JSON值的迭代器
+fn stream_into_iter() {
+    let data = "{\"k\": 3}1\"cool\"\"stuff\" 3{}  [0, 1, 2]";
+    let stream = Deserializer::from_str(data).into_iter::<Value>();
+    for value in stream {
+        println!("{}", value.unwrap())
+    }
 }
